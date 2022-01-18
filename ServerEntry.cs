2260276368933a3e51de89d7643091751ce5bcfb
Patrick <44092756+vigetious@users.noreply.github.com>
2022-01-18 20:18:29 +0000

    added support for shows not on user list and further extensions to logging

--- ServerEntry.cs
+++ ServerEntry.cs
@@ -43,13 +43,17 @@ public class ServerEntry : IServerEntryPoint {
                     String.Compare(anime.AlternativeTitles.En, episode.SeriesName, CultureInfo.CurrentCulture, CompareOptions.IgnoreCase | CompareOptions.IgnoreSymbols) == 0) {
                     _logger.LogInformation($"Found matching series: {anime.Title}");
                     UserAnimeListData detectedAnime = await GetAnime(anime.Id, Status.Watching);
-                    if (detectedAnime == null) {
+                    if (detectedAnime != null) {
+                        _logger.LogInformation($"Series ({anime.Title}) found on watching list");
+                        await UpdateAnimeStatus(detectedAnime, episode.IndexNumber);
+                        return;
+                    } else {
                         if (Plugin.Instance.PluginConfiguration.PlanToWatchOnly) {
                             // only search for shows that the user plans to watch
                             detectedAnime = await GetAnime(anime.Id, Status.Plan_to_watch);
                             if (detectedAnime != null) {
                                 _logger.LogInformation($"Series ({anime.Title}) found on plan to watch list");
-                                await UpdateAnimeWatchProgress(detectedAnime, episode.IndexNumber);
+                                await UpdateAnimeStatus(detectedAnime, episode.IndexNumber);
                                 return;
                             }
 
@@ -58,14 +62,17 @@ public class ServerEntry : IServerEntryPoint {
                         } else {
                             // do a general search for the show
                             detectedAnime = await GetAnime(anime.Id);
-                            if (detectedAnime.ListStatus.Status == Status.Completed && Plugin.Instance.PluginConfiguration.RewatchCompleted) {
+                            if (detectedAnime != null && detectedAnime.ListStatus.Status == Status.Completed && Plugin.Instance.PluginConfiguration.RewatchCompleted) {
                                 // user has already watched the show, and wants the show to be set as re-watching as per config
+                                await UpdateAnimeStatus(detectedAnime, episode.IndexNumber, true);
+                                return;
+                            } else {
+                                // show is not on the users list at all. must be a new series, add it to the watching list.
+                                _logger.LogInformation($"Series ({anime.Title}) not on user list");
+                                await UpdateAnimeStatus(new UserAnimeListData { Anime = anime }, episode.IndexNumber);
+                                return;
                             }
                         }
-                    } else {
-                        _logger.LogInformation($"Series ({anime.Title}) found on watching list");
-                        await UpdateAnimeWatchProgress(detectedAnime, episode.IndexNumber);
-                        return;
                     }
                 }
             }
@@ -74,6 +81,12 @@ public class ServerEntry : IServerEntryPoint {
         }
     }
 
+    /// <summary>
+    /// Get a single result from a user anime search.
+    /// </summary>
+    /// <param name="animeId">ID of the anime you want to get.</param>
+    /// <param name="status">User status of the show.</param>
+    /// <returns>Single anime result.</returns>
     private async Task<UserAnimeListData> GetAnime(int animeId, Status? status = null) {
         List<UserAnimeListData> watchingAnime = await _malApiCalls.GetUserAnimeList(status, idSearch: animeId);
 
@@ -86,30 +99,49 @@ public class ServerEntry : IServerEntryPoint {
         return null;
     }
 
-    private async Task UpdateAnimeWatchProgress(UserAnimeListData detectedAnime, int? indexNumber) {
-        if (indexNumber != null && detectedAnime.ListStatus.NumEpisodesWatched < indexNumber.Value) {
-            if (indexNumber.Value == detectedAnime.Anime.NumEpisodes) {
-                // user has reached the number of episodes in the show, set as completed
-                var response = await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Completed);
-                _logger.LogInformation($"Series ({detectedAnime.Anime.Title}) complete, marking show as complete in MAL");
-                if (detectedAnime.ListStatus.IsRewatching) {
-                    // also increase number of times re-watched by 1
-                    // only way to get the number of times re-watched is by doing the update and capturing the response, and then re-updating :/
-                    _logger.LogInformation($"Series ({detectedAnime.Anime.Title}) has also been re-watched, increasing re-watch count by 1");
-                    await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Completed, numberOfTimesRewatched: response.NumTimesRewatched + 1);
+    /// <summary>
+    /// Update a users anime status.
+    /// </summary>
+    /// <param name="detectedAnime">The anime search result to update.</param>
+    /// <param name="indexNumber">The episode number to update the anime to.</param>
+    private async Task UpdateAnimeStatus(UserAnimeListData detectedAnime, int? indexNumber, bool? setRewatching = null) {
+        if (indexNumber != null) {
+            if (detectedAnime.ListStatus != null) {
+                if (detectedAnime.ListStatus.NumEpisodesWatched < indexNumber.Value) {
+                    if (indexNumber.Value == detectedAnime.Anime.NumEpisodes) {
+                        // user has reached the number of episodes in the show, set as completed
+                        var response = await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Completed);
+                        _logger.LogInformation($"Series ({detectedAnime.Anime.Title}) complete, marking show as complete in MAL");
+                        if (detectedAnime.ListStatus.IsRewatching) {
+                            // also increase number of times re-watched by 1
+                            // only way to get the number of times re-watched is by doing the update and capturing the response, and then re-updating :/
+                            _logger.LogInformation($"Series ({detectedAnime.Anime.Title}) has also been re-watched, increasing re-watch count by 1");
+                            await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Completed, numberOfTimesRewatched: response.NumTimesRewatched + 1);
+                        }
+                    } else {
+                        if (detectedAnime.ListStatus.IsRewatching) {
+                            // MAL likes to mark re-watching shows as completed, instead of watching. I guess technically both are correct
+                            await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Completed);
+                        } else {
+                            if (setRewatching != null && setRewatching.Value) {
+                                _logger.LogInformation($"Series ({detectedAnime.Anime.Title}) has already been watched, marking show as re-watching");
+                                await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Watching, true);
+                            } else {
+                                await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Watching);
+                            }
+                        }
+                    }
+
+                    _logger.LogInformation($"Updated series ({detectedAnime.Anime.Title}) progress to {indexNumber.Value}");
+                } else {
+                    _logger.LogInformation("MAL reports episode already watched; not updating");
                 }
             } else {
-                if (detectedAnime.ListStatus.IsRewatching) {
-                    // MAL likes to mark re-watching shows as completed, instead of watching. I guess technically both are correct
-                    await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Completed);
-                } else {
-                    await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Watching);
-                }
+                // status is not set, must be a new show
+                _logger.LogInformation($"Adding new series ({detectedAnime.Anime.Title}) to user list as watching with a progress of {indexNumber.Value}");
+                await _malApiCalls.UpdateAnimeStatus(detectedAnime.Anime.Id, indexNumber.Value, Status.Watching);
+                // test this
             }
-
-            _logger.LogInformation($"Updated series ({detectedAnime.Anime.Title}) progress to {indexNumber.Value}");
-        } else {
-            _logger.LogInformation("MAL reports episode already watched; not updating");
         }
     }
 
