b35b2469c3cf722fd8a876c47cb6ddd8035c8de2
Patrick <44092756+vigetious@users.noreply.github.com>
2022-03-20 10:41:24 +0000

    added kitsu search anime function

---
+++ Api/Kitsu/KitsuApiCalls.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using jellyfin_ani_sync.Configuration;
+using jellyfin_ani_sync.Helpers;
+using jellyfin_ani_sync.Models;
+using jellyfin_ani_sync.Models.Kitsu;
+using MediaBrowser.Controller;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace jellyfin_ani_sync.Api.Kitsu; 
+
+public class KitsuApiCalls {
+    private readonly string ApiUrl = "https://kitsu.io/api/edge";
+    private readonly ILogger<KitsuApiCalls> _logger;
+    private readonly AuthApiCall _authApiCall;
+
+    public KitsuApiCalls(IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory, IServerApplicationHost serverApplicationHost, IHttpContextAccessor httpContextAccessor, UserConfig userConfig = null) {
+        _logger = loggerFactory.CreateLogger<KitsuApiCalls>();
+        _authApiCall = new AuthApiCall(ApiName.Kitsu, httpClientFactory, serverApplicationHost, httpContextAccessor, loggerFactory, userConfig: userConfig);
+    }
+    
+    public async Task<List<KitsuSearch.KitsuSearchData>> SearchAnime(string query) {
+        UrlBuilder url = new UrlBuilder {
+            Base = $"{ApiUrl}/anime"
+        };
+
+        if (query != null) {
+            query = StringFormatter.RemoveSpaces(query);
+            
+            url.Parameters.Add(new KeyValuePair<string, string>("filter[text]", query));
+        }
+
+        string builtUrl = url.Build();
+        _logger.LogInformation($"(Kitsu) Starting search for anime (GET {builtUrl})...");
+        var apiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Kitsu, MalApiCalls.CallType.GET, builtUrl);
+        if (apiCall != null) {
+            StreamReader streamReader = new StreamReader(await apiCall.Content.ReadAsStreamAsync());
+            var animeList = JsonSerializer.Deserialize<KitsuSearch.KitsuSearchMedia>(await streamReader.ReadToEndAsync());
+            
+            _logger.LogInformation("Search complete");
+            return animeList.KitsuSearchData;
+        }
+
+        return null;
+    }
+}
