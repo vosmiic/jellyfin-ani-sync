07618d547fd0976d5d71bb5361d6e00e8bae7a1f
Patrick <44092756+vigetious@users.noreply.github.com>
2022-03-25 16:59:20 +0000

    dotnet 5 namespace changes

--- Models/Kitsu/KitsuGetUser.cs
+++ Models/Kitsu/KitsuGetUser.cs
@@ -2,24 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace jellyfin_ani_sync.Models.Kitsu;
+namespace jellyfin_ani_sync.Models.Kitsu {
+    public class KitsuGetUser {
+        public class KitsuUser {
+            [JsonPropertyName("id")] public int Id { get; set; }
+            [JsonPropertyName("name")] public string Name { get; set; }
+            [JsonPropertyName("type")] public string Type { get; set; }
+        }
 
-public class KitsuGetUser {
-    public class KitsuUser {
-        [JsonPropertyName("id")] public int Id { get; set; }
-        [JsonPropertyName("name")] public string Name { get; set; }
-        [JsonPropertyName("type")] public string Type { get; set; }
-    }
+        public class KitsuUserData {
+            [JsonPropertyName("attributes")] public KitsuUser KitsuUser { get; set; }
 
-    public class KitsuUserData {
-        [JsonPropertyName("attributes")] public KitsuUser KitsuUser { get; set; }
+            [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+        }
 
-        [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-    }
-
-    public class KitsuUserRoot {
-        [JsonPropertyName("data")] public List<KitsuUserData> KitsuUserList { get; set; }
+        public class KitsuUserRoot {
+            [JsonPropertyName("data")] public List<KitsuUserData> KitsuUserList { get; set; }
+        }
     }
 }
