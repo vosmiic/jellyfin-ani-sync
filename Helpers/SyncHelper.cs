97fb6125a9303aba4cc0444f3ce7534ae98250a7
vosmiic <44092756+vosmiic@users.noreply.github.com>
2022-07-12 20:42:39 +0100

    made general sync helper

---
+++ Helpers/SyncHelper.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Jellyfin.Data.Enums;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Library;
+using Microsoft.Extensions.Logging;
+
+namespace jellyfin_ani_sync.Helpers; 
+
+public class SyncHelper {
+
+    public static List<BaseItem> GetUsersJellyfinLibrary(Guid userId, IUserManager userManager, ILibraryManager libraryManager) {
+        var query = new InternalItemsQuery(userManager.GetUserById(userId)) {
+            IncludeItemTypes = new[] {
+                BaseItemKind.Movie,
+                BaseItemKind.Series
+            },
+            IsVirtualItem = false
+        };
+
+        return libraryManager.GetItemList(query);
+    }
+
+    /// <summary>
+    /// Get the preferred series provider ID. 
+    /// </summary>
+    public static (AnimeOfflineDatabaseHelpers.Source? source, int? providerId) GetSeriesProviderId(Series series) {
+        if (int.TryParse(series.ProviderIds["AniList"], out int providerId)) {
+            return (AnimeOfflineDatabaseHelpers.Source.Anilist, providerId);
+        } else if (int.TryParse(series.ProviderIds["AniDB"], out providerId)) {
+            return (AnimeOfflineDatabaseHelpers.Source.Anidb, providerId);
+        }
+
+        return (null, null);
+    }
+    
+    /// <summary>
+    /// Get a list of seasons corresponding with their provider ID.
+    /// </summary>
+    /// <param name="convertedWatchList">List of metadata to match to seasons.</param>
+    /// <param name="providerId">AniList provider ID.</param>
+    /// <param name="series">Series to retrieve season of.</param>
+    /// <param name="source">Metadata source.</param>
+    /// <returns>List of seasons with their completed date and progress.</returns>
+    public static async Task<List<(Season, DateTime?, int)>> GetJellyfinSeasons(List<Sync.SyncAnimeMetadata> convertedWatchList,
+        int providerId,
+        Series series,
+        AnimeOfflineDatabaseHelpers.Source source,
+        ILogger logger,
+        ILoggerFactory loggerFactory,
+        IHttpClientFactory httpClientFactory,
+        IApplicationPaths applicationPaths) {
+        List<(Season, DateTime?, int)> seasons = new List<(Season, DateTime?, int)>();
+
+        List<AnimeListAnimeOfflineDatabaseCombo> seasonIdsFromJellyfin = await GetSeasonIdsFromJellyfin(providerId,
+            source,
+            logger,
+            loggerFactory,
+            httpClientFactory,
+            applicationPaths);
+        
+        foreach (AnimeListAnimeOfflineDatabaseCombo animeListAnime in seasonIdsFromJellyfin) {
+            var found = convertedWatchList.FirstOrDefault(item => int.TryParse(animeListAnime.AnimeListAnime.Anidbid, out int convertedAniDbId) && item.ids.AniDb == convertedAniDbId);
+            if (found != null) {
+                seasons.Add((series.Children.Select(season => season as Season).FirstOrDefault(season => season.IndexNumber == found.season), found.completedAt, found.episodesWatched));
+            }
+        }
+
+        return seasons;
+    }
+    
+    
+    /// <summary>
+    /// Get season IDs from Jellyfin library.
+    /// </summary>
+    /// <param name="providerId">ID of the provider.</param>
+    /// <returns>List of season IDs.</returns>
+    public static async Task<List<AnimeListAnimeOfflineDatabaseCombo>> GetSeasonIdsFromJellyfin(int providerId, AnimeOfflineDatabaseHelpers.Source source,
+        ILogger logger,
+        ILoggerFactory loggerFactory,
+        IHttpClientFactory httpClientFactory,
+        IApplicationPaths applicationPaths) {
+        var id = await AnimeOfflineDatabaseHelpers.GetProviderIdsFromMetadataProvider(httpClientFactory.CreateClient(NamedClient.Default), providerId, source);
+        if (id is { AniDb: { } }) {
+            var seasons = await AnimeListHelpers.ListAllSeasonOfAniDbSeries(logger, loggerFactory, httpClientFactory, applicationPaths, id.AniDb.Value);
+            var seasonsList = seasons.ToList();
+            if (seasonsList.Count() > 1) {
+                return seasonsList.Select(animeListAnime => new AnimeListAnimeOfflineDatabaseCombo { AnimeListAnime = animeListAnime }).ToList();
+            } else {
+                var xd = new List<AnimeListAnimeOfflineDatabaseCombo> {
+                    new() {
+                        AnimeListAnime = seasonsList.First(),
+                        OfflineDatabaseResponse = id
+                    }
+                };
+                return xd;
+            }
+        }
+
+        return null;
+    }
+
+    public class AnimeListAnimeOfflineDatabaseCombo {
+        public AnimeListHelpers.AnimeListAnime AnimeListAnime { get; set; }
+        public AnimeOfflineDatabaseHelpers.OfflineDatabaseResponse OfflineDatabaseResponse { get; set; }
+    }
+}
