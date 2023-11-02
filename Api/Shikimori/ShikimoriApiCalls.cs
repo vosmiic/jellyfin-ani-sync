4bba50540125061fd43f3eba62b1f4d38840bc76
vosmiic <44092756+vosmiic@users.noreply.github.com>
2023-11-02 19:53:30 -0400

    added updating user anime list

--- Api/Shikimori/ShikimoriApiCalls.cs
+++ Api/Shikimori/ShikimoriApiCalls.cs
@@ -7,16 +7,18 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using jellyfin_ani_sync.Configuration;
 using jellyfin_ani_sync.Helpers;
+using jellyfin_ani_sync.JsonConverters;
 using jellyfin_ani_sync.Models;
 using jellyfin_ani_sync.Models.Shikimori;
 using MediaBrowser.Controller;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
-namespace jellyfin_ani_sync.Api.Shikimori; 
+namespace jellyfin_ani_sync.Api.Shikimori;
 
 public class ShikimoriApiCalls {
     private readonly ILogger<ShikimoriApiCalls> _logger;
@@ -32,12 +34,12 @@ public class ShikimoriApiCalls {
         _logger = loggerFactory.CreateLogger<ShikimoriApiCalls>();
         _authApiCall = new AuthApiCall(ApiName.Shikimori, httpClientFactory, serverApplicationHost, httpContextAccessor, loggerFactory, userConfig: userConfig);
     }
-    
+
     public class User {
         [JsonPropertyName("id")] public int Id { get; set; }
         [JsonPropertyName("nickname")] public string Name { get; set; }
     }
-    
+
     /// <summary>
     /// Get a users information.
     /// </summary>
@@ -81,25 +83,131 @@ public class ShikimoriApiCalls {
             Base = $"{_apiBaseUrl}/animes"
         };
         url.Parameters.Add(new KeyValuePair<string, string>("search", searchString));
-        
-        return await PagedCall<ShikimoriMedia>(url, AuthApiCall.CallType.GET);
-    }
-    
-        private async Task<List<T>?> PagedCall<T>(UrlBuilder url, AuthApiCall.CallType callType) {
-        int page = 1;
-        url.Parameters.Add(new KeyValuePair<string, string>("limit", "50"));
-        url.Parameters.Add(new KeyValuePair<string, string>("page", page.ToString()));
 
-        var apiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, callType, url.Build(), requestHeaders: _requestHeaders);
+        return await PagedCall<ShikimoriMedia>(url, 50);
+    }
+
+
+    /// <summary>
+    /// Get an anime.
+    /// </summary>
+    /// <param name="id">ID of the anime you want to get.</param>
+    /// <returns></returns>
+    public async Task<ShikimoriMedia?> GetAnime(int id) {
+        UrlBuilder url = new UrlBuilder {
+            Base = $"{_apiBaseUrl}/animes/{id}"
+        };
+
+        HttpResponseMessage? apiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, AuthApiCall.CallType.GET, url.Build(), requestHeaders: _requestHeaders);
         if (apiCall == null) {
             return null;
         }
+
+        try {
+            StreamReader streamReader = new StreamReader(await apiCall.Content.ReadAsStreamAsync());
+            return JsonSerializer.Deserialize<ShikimoriMedia>(await streamReader.ReadToEndAsync());
+        } catch (Exception e) {
+            _logger.LogError($"Could not deserialize anime, reason: {e.Message}");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Get a users anime list.
+    /// </summary>
+    /// <param name="id">Only retrieve user progress of a single anime by ID.</param>
+    /// <returns></returns>
+    public async Task<List<ShikimoriUpdate.UserRate>?> GetUserAnimeList(int? id = null) {
+        UrlBuilder url = new UrlBuilder {
+            Base = $"{_apiBaseUrl}/v2/user_rates"
+        };
+
+        string? shikimoriUserId = await GetUserId();
+        if (shikimoriUserId == null) return null;
+        
+        url.Parameters.Add(new KeyValuePair<string, string>("user_id", shikimoriUserId));
+        if (id != null) {
+            url.Parameters.Add(new KeyValuePair<string, string>("target_id", id.Value.ToString()));
+            url.Parameters.Add(new KeyValuePair<string, string>("target_type", "Anime"));
+        }
+        
+        HttpResponseMessage? apiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, AuthApiCall.CallType.GET, url.Build(), requestHeaders: _requestHeaders);
+        if (apiCall == null) {
+            return null;
+        }
+
+        try {
+            StreamReader streamReader = new StreamReader(await apiCall.Content.ReadAsStreamAsync());
+            return JsonSerializer.Deserialize<List<ShikimoriUpdate.UserRate>>(await streamReader.ReadToEndAsync());
+        } catch (Exception e) {
+            _logger.LogError($"Could not deserialize user anime list, reason: {e.Message}");
+            return null;
+        }
+    }
+
+    public async Task<bool> UpdateAnime(int id, ShikimoriUpdate.UpdateStatus updateStatus, int progress, int? numberOfTimesRewatched = null) {
+        UrlBuilder url = new UrlBuilder {
+            Base = $"{_apiBaseUrl}/v2/user_rates"
+        };
+
+        string? shikimoriUserId = await GetUserId();
+        if (shikimoriUserId == null) return false;
+        
+        ShikimoriUpdate.UpdateBody updateBody = new ShikimoriUpdate.UpdateBody {
+            UserRate = new ShikimoriUpdate.UserRate {
+                AnimeId = id,
+                UserId = int.Parse(shikimoriUserId),
+                Episodes = progress,
+                Status = updateStatus
+            }
+        };
+
+        if (numberOfTimesRewatched != null) {
+            updateBody.UserRate.Rewatches = numberOfTimesRewatched.Value;
+        }
+
+        var jsonSerializerOptions = new JsonSerializerOptions {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Converters = { new IntToStringConverter.IntToStringJsonConverter() }
+        };
+        
+        var stringContent = new StringContent(JsonSerializer.Serialize(updateBody, jsonSerializerOptions), Encoding.UTF8, "application/json");
+
+        // get users current watch status of anime
+        var currentAnimeStatus = await GetUserAnimeList(id);
+        if (currentAnimeStatus == null) return false;
+        AuthApiCall.CallType callType;
+        if (currentAnimeStatus.Count == 0 || currentAnimeStatus.FirstOrDefault(ur => ur.Id == id) == null) {
+            callType = AuthApiCall.CallType.POST;
+        } else {
+            callType = AuthApiCall.CallType.PATCH;
+        }
+        // wait before making next call
+        await Task.Delay(1000);
+        HttpResponseMessage? response = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, callType, url.Build(), stringContent: stringContent, requestHeaders: _requestHeaders);
+        if (response != null) {
+            return response.IsSuccessStatusCode;
+        }
+
+        return false;
+    }
+
+    private async Task<List<T>?> PagedCall<T>(UrlBuilder url, int pageSize) {
+        int page = 1;
+        url.Parameters.Add(new KeyValuePair<string, string>("limit", pageSize.ToString()));
+        url.Parameters.Add(new KeyValuePair<string, string>("page", page.ToString()));
+
+        var apiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, AuthApiCall.CallType.GET, url.Build(), requestHeaders: _requestHeaders);
+        if (apiCall == null) {
+            return null;
+        }
+
         List<T>? result;
         try {
             StreamReader streamReader = new StreamReader(await apiCall.Content.ReadAsStreamAsync());
             result = JsonSerializer.Deserialize<List<T>>(await streamReader.ReadToEndAsync());
         } catch (Exception e) {
-            _logger.LogError($"Could not deserialize anime, reason: {e.Message}");
+            _logger.LogError($"Could not deserialize result, reason: {e.Message}");
             return null;
         }
 
@@ -109,15 +217,15 @@ public class ShikimoriApiCalls {
 
             url.Parameters.RemoveAll(item => item.Key == "page");
             url.Parameters.Add(new KeyValuePair<string, string>("page", page.ToString()));
-            
-            HttpResponseMessage? pageApiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, callType, url.Build());
+
+            HttpResponseMessage? pageApiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, AuthApiCall.CallType.GET, url.Build());
             if (pageApiCall == null) break;
             List<T>? nextPageResult;
             try {
                 StreamReader streamReader = new StreamReader(await apiCall.Content.ReadAsStreamAsync());
                 nextPageResult = JsonSerializer.Deserialize<List<T>>(await streamReader.ReadToEndAsync());
             } catch (Exception e) {
-                _logger.LogError($"Could not retrieve next page of anime, reason: {e.Message}");
+                _logger.LogError($"Could not retrieve next result page, reason: {e.Message}");
                 break;
             }
 
@@ -132,27 +240,19 @@ public class ShikimoriApiCalls {
         return result;
     }
 
-    /// <summary>
-    /// Get an anime.
-    /// </summary>
-    /// <param name="id">ID of the anime you want to get.</param>
-    /// <returns></returns>
-    public async Task<ShikimoriMedia?> GetAnime(int id) {
-        UrlBuilder url = new UrlBuilder {
-            Base = $"{_apiBaseUrl}/animes/{id}"
-        };
-        
-        var apiCall = await _authApiCall.AuthenticatedApiCall(ApiName.Shikimori, AuthApiCall.CallType.GET, url.Build(), requestHeaders: _requestHeaders);
-        if (apiCall == null) {
-            return null;
+    private async Task<string?> GetUserId() {
+        string? shikimoriUserId = _userConfig?.KeyPairs.FirstOrDefault(keypair => keypair.Key == "ShikimoriUserId")?.Value;
+        if (shikimoriUserId == null || string.IsNullOrEmpty(shikimoriUserId))  {
+            _logger.LogInformation("No Shikimori user ID stored in the config. Attempting to retrieve...");
+            User? user = await GetUserInformation();
+            if (user != null) {
+                shikimoriUserId = user.Id.ToString();
+            } else {
+                _logger.LogError("Could not retrieve Shikimori user ID");
+                return null;
+            }
         }
 
-        try {
-            StreamReader streamReader = new StreamReader(await apiCall.Content.ReadAsStreamAsync());
-            return JsonSerializer.Deserialize<ShikimoriMedia>(await streamReader.ReadToEndAsync());
-        } catch (Exception e) {
-            _logger.LogError($"Could not deserialize anime, reason: {e.Message}");
-            throw;
-        }
+        return shikimoriUserId;
     }
 }
