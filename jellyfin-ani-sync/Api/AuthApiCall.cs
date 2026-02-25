05552a61cb674973242043f3c58aaf5b91bb2f93
vosmiic <44092756+vosmiic@users.noreply.github.com>
2026-02-25 19:45:49 +0000

    cleanup authenticated api calls

--- jellyfin-ani-sync/Api/AuthApiCall.cs
+++ jellyfin-ani-sync/Api/AuthApiCall.cs
@@ -17,60 +17,46 @@ using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace jellyfin_ani_sync.Api {
-    public class AuthApiCall {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IServerApplicationHost _serverApplicationHost;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly ILoggerFactory _loggerFactory;
-        private readonly ILogger<AuthApiCall> _logger;
-        private readonly IMemoryCache _memoryCache;
-        private readonly IAsyncDelayer _delayer;
-        public UserConfig UserConfig { get; set; }
-        public static int defaultTimeoutSeconds = 5;
-        public static int timeoutIncrementMultiplier = 2;
-
-        public AuthApiCall(IHttpClientFactory httpClientFactory,
-            IServerApplicationHost serverApplicationHost,
-            IHttpContextAccessor httpContextAccessor,
-            ILoggerFactory loggerFactory,
-            IMemoryCache memoryCache,
-            IAsyncDelayer delayer,
-            UserConfig userConfig) {
-            _httpClientFactory = httpClientFactory;
-            _serverApplicationHost = serverApplicationHost;
-            _httpContextAccessor = httpContextAccessor;
-            _loggerFactory = loggerFactory;
-            _logger = loggerFactory.CreateLogger<AuthApiCall>();
-            _memoryCache = memoryCache;
-            _delayer = delayer;
-            UserConfig = userConfig;
-        }
+    public class AuthApiCall (
+        IHttpClientFactory httpClientFactory,
+        IServerApplicationHost serverApplicationHost,
+        IHttpContextAccessor httpContextAccessor,
+        ILoggerFactory loggerFactory,
+        IMemoryCache memoryCache,
+        IAsyncDelayer delayer,
+        UserConfig userConfig) {
+        private readonly ILogger<AuthApiCall> _logger = loggerFactory.CreateLogger<AuthApiCall>();
+        private UserConfig UserConfig { get; } = userConfig;
+        private const int DefaultTimeoutSeconds = 5;
+        private const int TimeoutIncrementMultiplier = 2;
 
         /// <summary>
         /// Make an authenticated API call.
         /// </summary>
+        /// <param name="provider">Provider to make the authenticated API call to.</param>
         /// <param name="callType">The type of call to make.</param>
         /// <param name="url">The URL that you want to make the request to.</param>
         /// <param name="formUrlEncodedContent">The form data to be posted.</param>
+        /// <param name="stringContent">Content in string format to include int the API call.</param>
+        /// <param name="requestHeaders">Request headers to include in the API call.</param>
         /// <returns>API call response.</returns>
         /// <exception cref="NullReferenceException">Authentication details not found.</exception>
         /// <exception cref="Exception">Non-200 response.</exception>
-        /// <exception cref="AuthenticationException">Could not authenticate with the API.</exception>
-        public async Task<HttpResponseMessage?> AuthenticatedApiCall(ApiName provider, CallType callType, string url, FormUrlEncodedContent formUrlEncodedContent = null, StringContent stringContent = null, Dictionary<string, string>? requestHeaders = null) {
+        public async Task<HttpResponseMessage?> AuthenticatedApiCall(ApiName provider, CallType callType, string url, FormUrlEncodedContent? formUrlEncodedContent = null, StringContent? stringContent = null, Dictionary<string, string>? requestHeaders = null) {
             int attempts = 0;
-            int timeoutSeconds = defaultTimeoutSeconds;
+            int timeoutSeconds = DefaultTimeoutSeconds;
             UserApiAuth? auth = UserConfig.UserApiAuth?.FirstOrDefault(item => item.Name == provider);
             if (auth == null) {
-                _logger.LogError("Could not find authentication details, please authenticate the plugin first");
+                _logger.LogError("({ApiName}) Could not find authentication details, please authenticate the plugin first", provider);
                 return null;
             }
 
-            var client = _httpClientFactory.CreateClient(NamedClient.Default);
-            DateTime lastCallDateTime = _memoryCache.Get<DateTime>(MemoryCacheHelper.GetLastCallDateTimeKey(provider));
+            var client = httpClientFactory.CreateClient(NamedClient.Default);
+            DateTime lastCallDateTime = memoryCache.Get<DateTime>(MemoryCacheHelper.GetLastCallDateTimeKey(provider));
             if (lastCallDateTime != default)
             {
-                _logger.LogDebug($"({provider}) Delaying API call to prevent 429 (too many requests)...");
-                await _delayer.Delay(DateTime.UtcNow.Subtract(lastCallDateTime));
+                _logger.LogDebug("({ApiName}) Delaying API call to prevent 429 (too many requests)...", provider);
+                await delayer.Delay(DateTime.UtcNow.Subtract(lastCallDateTime));
             }
             while (attempts < 3) {
                 if (requestHeaders != null) {
@@ -82,7 +68,7 @@ namespace jellyfin_ani_sync.Api {
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", auth.AccessToken);
                 HttpResponseMessage responseMessage = new HttpResponseMessage();
                 try {
-                    _memoryCache.Set(MemoryCacheHelper.GetLastCallDateTimeKey(provider), DateTime.UtcNow, TimeSpan.FromSeconds(5));
+                    memoryCache.Set(MemoryCacheHelper.GetLastCallDateTimeKey(provider), DateTime.UtcNow, TimeSpan.FromSeconds(5));
                     switch (callType) {
                         case CallType.GET:
                             responseMessage = await client.GetAsync(url);
@@ -104,43 +90,43 @@ namespace jellyfin_ani_sync.Api {
                             break;
                     }
                 } catch (Exception e) {
-                    _logger.LogError(e.Message);
+                    _logger.LogError("({ApiName} Could not make authenticated API call: {EMessage}", provider, e.Message);
                 }
 
 
                 if (responseMessage.IsSuccessStatusCode) {
                     return responseMessage;
-                } else {
-                    switch (responseMessage.StatusCode)
-                    {
-                        case HttpStatusCode.Unauthorized:
-                            // token has probably expired; try refreshing it
-                            UserApiAuth newAuth;
-                            try {
-                                newAuth = await new ApiAuthentication(provider, _httpClientFactory, _serverApplicationHost, _httpContextAccessor, _loggerFactory, _memoryCache).GetToken(UserConfig.UserId, refreshToken: auth.RefreshToken);
-                            } catch (Exception e) {
-                                _logger.LogError($"Could not re-authenticate: {e.Message}, please manually re-authenticate the user via the Ani-Sync configuration page");
-                                return null;
-                            }
+                }
 
-                            // and then make the call again, using the new auth details
-                            auth = newAuth;
-                            attempts++;
-                            break;
-                        case HttpStatusCode.TooManyRequests:
-                            _logger.LogWarning($"({provider}) API rate limit exceeded, retrying the API call again in {timeoutSeconds} seconds...");
-                            await _delayer.Delay(TimeSpan.FromSeconds(timeoutSeconds));
-                            timeoutSeconds *= timeoutIncrementMultiplier;
-                            attempts++;
-                            break;
-                        default:
-                            _logger.LogError($"Unable to complete {provider} API call ({callType.ToString()} {url}), reason: {responseMessage.StatusCode}, content: \n{await responseMessage.Content.ReadAsStringAsync()}");
+                switch (responseMessage.StatusCode)
+                {
+                    case HttpStatusCode.Unauthorized:
+                        // token has probably expired; try refreshing it
+                        UserApiAuth newAuth;
+                        try {
+                            newAuth = await new ApiAuthentication(provider, httpClientFactory, serverApplicationHost, httpContextAccessor, loggerFactory, memoryCache).GetToken(UserConfig.UserId, refreshToken: auth.RefreshToken);
+                        } catch (Exception e) {
+                            _logger.LogError("({ApiName}) Could not re-authenticate: {EMessage}, please manually re-authenticate the user via the Ani-Sync configuration page", provider, e.Message);
                             return null;
-                    }
+                        }
+
+                        // and then make the call again, using the new auth details
+                        auth = newAuth;
+                        attempts++;
+                        break;
+                    case HttpStatusCode.TooManyRequests:
+                        _logger.LogWarning("({ApiName}) API rate limit exceeded, retrying the API call again in {TimeoutSeconds} seconds...", provider, timeoutSeconds);
+                        await delayer.Delay(TimeSpan.FromSeconds(timeoutSeconds));
+                        timeoutSeconds *= TimeoutIncrementMultiplier;
+                        attempts++;
+                        break;
+                    default:
+                        _logger.LogError("({ApiName}) Unable to complete {Provider} API call ({ToString} {Url}), reason: {ResponseMessageStatusCode}, content: \n{ReadAsStringAsync}", provider, provider, callType.ToString(), url, responseMessage.StatusCode, await responseMessage.Content.ReadAsStringAsync());
+                        return null;
                 }
             }
 
-            _logger.LogError("Unable to authenticate the API call, re-authenticate the plugin");
+            _logger.LogError("({ApiName}) Unable to authenticate the API call, re-authenticate the plugin", provider);
             return null;
         }
         
